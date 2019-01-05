b7e7e24578c8a7dfadc4956c635471e4b31d2b29
Jian-Huan Hong <jhong@ancestry.com>
2019-01-04 18:41:48 -0800

    Moving the Amount Calculation from Customer to Rental

--- src/main/java/bad/robot/refactoring/Rental.java
+++ src/main/java/bad/robot/refactoring/Rental.java
@@ -24,4 +24,23 @@ public class Rental {
         return daysRented;
     }
 
+    public double amountFor() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
