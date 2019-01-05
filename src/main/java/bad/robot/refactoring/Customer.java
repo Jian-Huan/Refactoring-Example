d277081e134390fab963c04241b43e706d90f3eb
Jian-Huan Hong <jhong@ancestry.com>
2019-01-04 21:50:43 -0800

    Replace Temp with Query to remove the redundant variable, thisAmount.

--- src/main/java/bad/robot/refactoring/Customer.java
+++ src/main/java/bad/robot/refactoring/Customer.java
@@ -30,19 +30,17 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
-            double thisAmount = 0;
-            thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
+
             // add bonus for a two day new release rental
             if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
 
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
 
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
