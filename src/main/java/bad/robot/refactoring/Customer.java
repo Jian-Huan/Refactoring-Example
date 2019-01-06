24e418153e64aada8f0a4cd70a5d2383e14f79bb
Jian-Huan Hong <jhong@ancestry.com>
2019-01-05 18:14:36 -0800

    Removing Temps: totalAmount and frequentRenterPoints.

--- src/main/java/bad/robot/refactoring/Customer.java
+++ src/main/java/bad/robot/refactoring/Customer.java
@@ -25,27 +25,31 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-
         String result = "Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
-            // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-
-            totalAmount += each.getCharge();
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        for (Rental each : rentals) {
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        for (Rental each : rentals) {
+            result += each.getCharge();
+        }
         return result;
     }
 
